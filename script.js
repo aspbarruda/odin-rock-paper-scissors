0726af7a88307f8da98cdfc6de496434f19247b9
Andre <aspbarruda@gmail.com>
2022-10-19 20:31:21 -0300

    Modified to add contest with multiple rounds

--- script.js
+++ script.js
@@ -13,60 +13,86 @@ function getComputerChoice() {
     return move;
 }
 
-function getResult(playerSelection, computerSelection) {
-    let message;
+function playRound(playerSelection, computerSelection) {
+    let result;
     if (playerSelection === 'rock') {
         if (computerSelection === 'rock') {
-            message = 'It\'s a Tie';
+            result = 0;
+            return result;
         }
         else if (computerSelection === 'paper') {
-            message = 'You lose! Paper beats rock!';
+            result = -1;
+            return result;
         }
         else {
-            message = 'You win! Rock beats scissors!';
+            result = 1;
+            return result;
         }
     }
 
     if (playerSelection === 'scissors') {
         if (computerSelection === 'scissors') {
-            message = 'It\'s a Tie';
+            result = 0;
+            return result;
         }
         else if (computerSelection === 'rock') {
-            message = 'You lose! Rock beats scissors!';
+            result = -1;
+            return result;
         }
         else {
             message = 'You win! Scissors beats paper!';
+            result = 1;
+            return result;
         }
     }
 
     if (playerSelection === 'paper') {
         if (computerSelection === 'paper') {
-            message = 'It\'s a Tie';
+            result = 0;
+            return result;
         }
         else if (computerSelection === 'scissors') {
-            message = 'You lose! Scissors beats paper!';
+            result = -1;
+            return result;
         }
         else {
-            message = 'You win! Paper beats rock!';
+            result = 1;
+            return result;
         }
     }
     return message;
 }
 
-let keepGoing = true;
-let playerSelection;
+function getUserInput() {
+    let keepGoing = true;
+    let playerSelection;
 
-while (keepGoing) {
-    playerSelection = prompt("Enter your move: ").toLowerCase();
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-        keepGoing = false;
+    while (keepGoing) {
+        playerSelection = prompt("Enter your move: ").toLowerCase();
+        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+            keepGoing = false;
+        }
+        else {
+            alert("Must enter valid choice!");
+        }
     }
-    else {
-        alert("Must enter valid choice!");
-    }
+    return playerSelection;
 }
 
-let computerSelection = getComputerChoice();
-let message = getResult(playerSelection, computerSelection);
-console.log(computerSelection);
-console.log(message);
+let result = 0;
+
+for (let i = 0; i < 5; i++) {
+    let playerSelection = getUserInput();
+    let computerSelection = getComputerChoice();
+    result += playRound(playerSelection, computerSelection);
+}
+
+if (result > 0) {
+    console.log("You won!");
+}
+else if (result < 0) {
+    console.log("You lost!");
+}
+else {
+    console.log("It was a tie after all!");
+}
